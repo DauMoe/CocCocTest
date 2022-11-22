19d64a1d35f084ab0c2588dbb8a9f34e6b6d2564
Vinh Nguyen <vinhnn@cmcglobal.vn>
2022-11-22 12:19:03 +0700

    First solution, reduce about 5 times

--- test1/index.js
+++ test1/index.js
@@ -20,7 +20,7 @@ async function runApp() {
 function handmadeIntersectionObserver() {
   const lastItemFirstRender = document.getElementsByClassName("item")[last-1];
   const rect = lastItemFirstRender.getBoundingClientRect();
-  const visibleInViewport = rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  const visibleInViewport = rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 100 && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
   if (visibleInViewport) {
     runApp();
   }
--- test2/index.js
+++ test2/index.js
@@ -2,14 +2,36 @@ let total = 0;
 const MaxAge = 200;
 
 console.clear();
+console.log("running...");
 function rangeCount(ages, ranges) {
+  ages = ages.sort((a, b) => a-b);
+  const agesObject = {};
+  for (const age of ages) {
+    if (age in agesObject) {
+      agesObject[age]++;
+    } else {
+      agesObject[age] = 1;
+    }
+  }
+  const x = [];
+  for (const range of ranges) {
+    let a = 0;
+    for (let i = range[0]; i <= range[1]; i++) {
+      a = a + (range[i]>>>0);
+    }
+    x.push(a);
+  }
+  return x;
+  
   return ranges.map(range =>
     ages.reduce((people, age) =>
-    people + ((age >= range[0] && age<=range[1] && 1) || 0), 0));
+      people + ((age >= range[0] && age<=range[1] && 1) || 0)
+    , 0)
+  );
 }
 
 function generateData() {
-
+/*
 //uncomment to receive sample data
 {
   const ages = [3, 5, 44, 7, 33, 33, 17, 90, 8, 12, 3, 24, 120, 7, 100]; // will be generated
@@ -23,14 +45,15 @@ function generateData() {
     ranges,
   };
 }
-
+*/
+//10_000_000
   const ages = Array(1000)
     .fill("")
     .map(() => {
       return Math.floor(Math.random() * MaxAge);
     });
 
-  const ranges = Array(10000000)
+  const ranges = Array(10_000_0)
     .fill("")
     .map(() => {
       return [
